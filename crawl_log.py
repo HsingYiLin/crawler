7524bd4450ef4afe6801df43fa34fdfce69c4256
Hsingyi Lin <hsingyilin@HsingyideMacBook-Pro.local>
2023-09-28 19:10:54 +0800

    條件判斷優化

--- crawl_log.py
+++ crawl_log.py
@@ -20,8 +20,8 @@ def reset_crawl_times():
     maxdb = getMysqlConnect()
     cursor = maxdb.cursor()
     # file_path = r'C:\igquery\crawl\ip.json'
-    file_path = r'/home/hsingyi/igquery/crawl/ip.json'
-    # file_path = r'/root/igquery/crawl/ip.json'
+    # file_path = r'/home/hsingyi/igquery/crawl/ip.json'
+    file_path = r'/root/igquery/crawl/ip.json'
     with open(file_path, 'r') as json_file:
         ipJson = json.load(json_file)
     for ip in ipJson:
@@ -32,43 +32,18 @@ def reset_crawl_times():
         crawl_times_day = row[4]
         crawl_times_week = row[5]
         last_record = f"上週:{crawl_times_week},昨天:{crawl_times_day},前小時:{crawl_times_hour}"
-
-        #重製(週一0點)
+        sql = ''
+        # #重製(週一0點)
         if day_of_week == 0 and current_hour == 0 and current_minute == 0:
-            cursor.execute(
-                f"""
-                UPDATE crawl_log SET 
-                `crawl_times_hour`=0, 
-                `crawl_times_day`=0, 
-                `crawl_times_week`=0,
-                `last_record` ='{last_record}'
-                WHERE `ip_mark`='{ip_mark}'
-                """
-            )
-            maxdb.commit()
+            sql = " `crawl_times_hour`=0, `crawl_times_day`=0, `crawl_times_week`=0,"
         # #重製(每天0點)
         elif current_hour == 0 and current_minute == 0:
-            cursor.execute(
-                f"""
-                UPDATE crawl_log SET 
-                `crawl_times_hour`=0, 
-                `crawl_times_day`=0, 
-                `last_record` ='{last_record}'
-                WHERE `ip_mark`='{ip_mark}'
-                """
-            )
-            maxdb.commit()
+            sql = " `crawl_times_hour`=0, `crawl_times_day`=0,"
         # #重製(整點)
-        elif current_minute == 0:
-            cursor.execute(
-                f"""
-                UPDATE crawl_log SET 
-                `crawl_times_hour`=0, 
-                `last_record` ='{last_record}'
-                WHERE `ip_mark`='{ip_mark}'
-                """
-            )
-            maxdb.commit()
+        elif current_minute == 6:
+            sql = " `crawl_times_hour`=0,"
+        cursor.execute(f"UPDATE crawl_log SET {sql} `last_record` ='{last_record}' WHERE `ip_mark`='{ip_mark}'")
+        maxdb.commit()
     cursor.close()
     maxdb.close()
 
